c97f964198fe870015f629f8eb563e73fefff4ab
Hayley <haowens@email.unc.edu>
2024-07-23 14:30:30 -0500

    appears to work

--- parserator_web/views.py
+++ parserator_web/views.py
@@ -4,7 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
 from rest_framework.exceptions import ParseError
-
+import json
 
 class Home(TemplateView):
     template_name = 'parserator_web/index.html'
@@ -16,9 +16,30 @@ class AddressParse(APIView):
     def get(self, request):
         # TODO: Flesh out this method to parse an address string using the
         # parse() method and return the parsed components to the frontend.
-        return Response({})
+
+        # TODO: handle strings that cannot be parsed/return errors
+        address = request.query_params.get('address', '')
+
+        try:
+            parsed_components, address_type = self.parse(address)
+        
+            return Response({
+                'input_string': address,
+                'address_components': parsed_components,
+                'address_type': address_type
+            })
+        except Exception as e:
+            return Response({'error': 'could not parse'})
+
+        
 
     def parse(self, address):
         # TODO: Implement this method to return the parsed components of a
         # given address using usaddress: https://github.com/datamade/usaddress
-        return address_components, address_type
+        try:
+            print(address)
+            address_components = usaddress.tag(address)
+            address_type = address_components[1]
+            return address_components[0], address_type
+        except usaddress.RepeatedLabelError as e:
+            raise e
